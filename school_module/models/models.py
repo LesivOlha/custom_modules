12a567919752adb31055d68f3d832e0e01cef892
LesivOlha <lesivolga98@gmail.com>
2022-03-21 19:29:38 +0200

    update module

--- school_module/models/models.py
+++ school_module/models/models.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class schoolModule(models.Model):
     _name = 'school.module'
     _description = 'school_module'
 
-    name = fields.Char()
+    @api.depends('name')
+    def one_changes(self):
+        for record in self:
+            if record.name:
+                record.lname = 'add a comment ' + record.name
+
+    name = fields.Char('Hello')
+    lname = fields.Char('add a comment', compute=one_changes, store=True)
     type = fields.Selection([('school', 'School'),
                              ('kindergarten', 'Kindergarten'),
                              ('institute', 'Institute')])
     address = fields.Char()
+    phone = fields.Char()
     people_ids = fields.One2many('people.module', 'institution_id')
     description = fields.Text()
 
@@ -22,12 +31,33 @@ class peopleModule(models.Model):
 
     name = fields.Char()
     last_name = fields.Char()
-    date = fields.Date()
+    date = fields.Integer()
+    text = fields.Text()
+    good_date = fields.Integer(default=18)
     type = fields.Selection([('school', 'School'),
                              ('kindergarten', 'Kindergarten'),
                              ('institute', 'Institute')])
     institution_id = fields.Many2one('school.module')
     group = fields.Char()
+
+    def _auto_text(self):
+        for rec in self.search([]):
+            if rec.text:
+                rec.text += "Слава Україні"
+            else:
+                rec.text = "Слава Україні"
+
+    @api.constrains('date')
+    def two_changes(self):
+        for record in self:
+            if record.date < record.good_date:
+                raise ValidationError("You can't study here")
+
+    @api.onchange('name')
+    def tree_changes(self):
+        for record in self:
+            record.last_name = record.name
+
 #
 #     @api.depends('value')
 #     def _value_pc(self):
