a0048be6311fd76ed42d9932c47866f7c2338e11
LesivOlha <lesivolga98@gmail.com>
2022-03-22 17:44:47 +0200

    exchanges module

--- city_weather/__manifest__.py
+++ city_weather/__manifest__.py
@@ -28,6 +28,7 @@
         'wizard/upload.xml',
         'views/views.xml',
         'views/templates.xml',
+        'data/data.xml'
 
     ],
     # only loaded in demonstration mode
--- school_module/__manifest__.py
+++ school_module/__manifest__.py
@@ -27,7 +27,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/data.xml',
+        'views/cron.xml',
         'security/security.xml'
     ],
     # only loaded in demonstration mode
