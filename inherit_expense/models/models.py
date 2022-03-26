515ba21262c9c703e97e6043761d3ffa3c610a5e
LesivOlha <lesivolga98@gmail.com>
2022-03-26 17:59:19 +0200

    add function

--- inherit_expense/models/models.py
+++ inherit_expense/models/models.py
@@ -3,10 +3,29 @@
 from odoo import models, fields, api
 
 
-class inherit_expense(models.Model):
-     _inherit = 'hr.expense'
-     _description = 'inherit_expense'
+class InheritExpense(models.Model):
+    _inherit = 'hr.expense'
+    _description = 'inherit_expense'
 
-     test = fields.Char()
-     mort = fields.Text()
-     casa = fields.Char()
+    test = fields.Char()
+    mort = fields.Text()
+    casa = fields.Char()
+
+
+    def write(self, vals):
+        vals['casa'] = 'Casa'
+        res = super(InheritExpense, self).write(vals)
+        return res
+
+    @api.model
+    def create(self, vals):
+        vals['test'] = 'testing'
+        res = super(InheritExpense, self).create(vals)
+        return res
+
+
+    @api.model
+    def default_get(self, vals):
+        res = super(InheritExpense, self).default_get(vals)
+        res['mort'] = 'morty'
+        return res
